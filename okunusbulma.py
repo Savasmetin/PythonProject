aa1c908a7f2f5ac10c42fb34ecddc5ddb2a918f8
SAVAŞ METİNOĞLU <76447218+Savasmetin@users.noreply.github.com>
2023-03-25 19:27:53 +0300

    Add files via upload

---
+++ okunusbulma.py
@@ -0,0 +1,12 @@
+a= int(input("bir sayı giriniz"))
+b = int(input("bir sayı daha giriniz"))
+if  1 <= a < 10 and 0 <= b < 10:
+    pass
+else:
+    print("hatalı girdiniz")
+
+def okunusbulma(a,b):
+    dizia =["on","yirmi","otuz"]
+
+
+okunusbulma(a,b)
